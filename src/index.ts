4216f4c1a28245cfddcf0204f57e3e4b13ce0282
Yaser <me@yaser.ca>
2022-04-23 14:03:49 -0700

    Removed DatePicker

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ import 'styles/globals.css';
 export { Button, ButtonType, ButtonIcon } from './Components/Button';
 export { TextInput } from './Components/Input/TextInput';
 export { FileInput } from './Components/Input/FileInput';
-export { DatePicker } from './Components/Input/DatePicker';
