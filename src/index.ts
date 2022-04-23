f7c8fea10edcd3717f116cd5e29fb98c830c479c
Yaser <me@yaser.ca>
2022-04-23 14:08:06 -0700

    removed global css

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import 'styles/globals.css';
-
 export { Button, ButtonType, ButtonIcon } from './Components/Button';
 export { TextInput } from './Components/Input/TextInput';
 export { FileInput } from './Components/Input/FileInput';
