8949935ac678baa12986d56b631bf1076dc52b93
Yaser <me@yaser.ca>
2022-04-24 01:11:56 -0700

    Exported DatePicker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export { Button, ButtonType, ButtonIcon } from './Components/Button';
-export { TextInput } from './Components/Input/TextInput';
-export { FileInput } from './Components/Input/FileInput';
-export { Dropdown } from './Components/Input/Dropdown/Dropdown';
+export {
+  Button,
+  ButtonType,
+  ButtonIcon,
+  IButtonProps,
+} from './Components/Button';
+export { TextInput, ITextInputProps } from './Components/Input/TextInput';
+export { FileInput, IFileInputProps } from './Components/Input/FileInput';
+export { Dropdown, IDropdownProps } from './Components/Input/Dropdown/Dropdown';
+export {
+  DatePicker,
+  IDatePickerProps,
+} from './Components/DatePicker/DatePicker';
