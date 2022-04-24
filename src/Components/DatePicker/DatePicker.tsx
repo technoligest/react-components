ea852394cde35dc315581d70b634f09f950b12cd
Yaser <me@yaser.ca>
2022-04-24 01:39:50 -0700

    Fixed react import

--- src/Components/DatePicker/DatePicker.tsx
+++ src/Components/DatePicker/DatePicker.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import { DateTime } from 'luxon';
 import { Poppable } from '../Common/Poppable';
 import { DatePickerInternal } from './DatePickerInternal';
