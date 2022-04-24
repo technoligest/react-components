f07e954b3d575fbbedac2bbe89e19ceb7273c4a6
Yaser <me@yaser.ca>
2022-04-24 01:25:54 -0700

    Made datepicker poppable

--- src/Components/Common/Poppable.tsx
+++ src/Components/Common/Poppable.tsx
@@ -2,77 +2,54 @@ import { ModifierPhases } from '@popperjs/core';
 import * as React from 'react';
 
 import { Modifier, usePopper } from 'react-popper';
-import { Poppable } from '../../../Components/Common/Poppable';
-import { DropdownItem } from '../../../Components/Input/Dropdown/DropdownItem';
-import { DropdownListContainer } from '../../../Components/Input/Dropdown/DropdownListContainer';
-import { TextInput } from '../TextInput';
-import { IDropdownAnchorProps } from './DropdownAnchor';
-import { IDropdownItemProps } from './DropdownItem';
+import { DropdownAnchor } from '../../Components/Input/Dropdown/DropdownAnchor';
+import { IDropdownAnchorProps } from '../Input/Dropdown/DropdownAnchor';
 
 export interface IDropdownProps {
-  items: IDropdownItemProps[];
   anchor: IDropdownAnchorProps;
-  unfoundValue?: {
-    onAdd: (newValue: string) => void;
-    getLabel: (searchString: string) => string;
-  };
-  searchable?: boolean;
 }
 
-export const Dropdown: React.FunctionComponent<IDropdownProps> = props => {
+export const Poppable: React.FunctionComponent<
+  React.PropsWithChildren<IDropdownProps>
+> = props => {
   const [searchString, setSearchString] = React.useState('');
   const [isVisible, setIsVisible] = React.useState(false);
   const anchorReference = React.useRef(null);
   const popperRef = React.useRef(null);
+  const { styles, attributes } = usePopperInternal(anchorReference, popperRef);
 
   useCloseOnClickOutside(popperRef, anchorReference, setIsVisible);
   const firstItemRef: React.LegacyRef<HTMLDivElement> | undefined =
     React.useRef(null);
-  const dropdownItems = props.items
-    .filter(
-      item => item.text.toLowerCase().indexOf(searchString.toLowerCase()) !== -1
-    )
-    .map((item, i) => {
-      const ref = i === 0 ? firstItemRef : undefined;
-      return (
-        <DropdownItem
-          key={item.text}
-          {...item}
-          onClick={() => {
-            item.onClick();
-            setIsVisible(false);
-            setSearchString('');
-          }}
-          ref={ref}
-          text={item.text}
-        />
-      );
-    });
   return (
-    <Poppable anchor={props.anchor}>
-      <DropdownListContainer isVisible={isVisible}>
-        {props.searchable && (
-          <TextInput
-            type='text'
-            value={searchString}
-            onChange={setSearchString}
-          />
-        )}
-        {dropdownItems}
-        {props.unfoundValue &&
-          searchString &&
-          !props.items.find(item => item.text === searchString) && (
-            <DropdownItem
-              onClick={() => {
-                props.unfoundValue?.onAdd(searchString);
-                setIsVisible(false);
-                setSearchString('');
-              }}
-              text={props.unfoundValue.getLabel(searchString)}
-            />
-          )}
-      </DropdownListContainer>
-    </Poppable>
+    <div>
+      <DropdownAnchor
+        outerProps={props.anchor}
+        innerProps={{
+          setIsVisible: newIsVisible => {
+            setIsVisible(newIsVisible);
+            if (newIsVisible) {
+              if (firstItemRef.current) {
+                (firstItemRef.current as any).focus();
+              }
+            }
+          },
+          isVisible,
+          anchorReference,
+        }}
+      />
+      <div
+        ref={popperRef}
+        style={{
+          ...styles.popper,
+          zIndex: 1000,
+          marginBottom: '5px !important',
+        }}
+        {...attributes.popper}
+      >
+        {props.children}
+      </div>
+    </div>
   );
 };
 
--- src/Components/DatePicker/DatePicker.tsx
+++ src/Components/DatePicker/DatePicker.tsx
@@ -1,61 +1,19 @@
+import * as React from 'React';
 import { DateTime } from 'luxon';
-import * as React from 'react';
-import { DatePickerDayView } from './DatePickerDayView';
-import { DatePickerHeader } from './DatePickerHeader';
-import { DatePickerHelperButtons } from './DatePickerHelperButtons';
-import { DatePickerMonthView } from './DatePickerMonthView';
-import { TDatePickerView } from './DatePickerTypes';
-import { DatePickerYearView } from './DatePickerYearView';
+import { Poppable } from '../Common/Poppable';
+import { DatePickerInternal } from './DatePickerInternal';
+import { IDropdownAnchorProps } from '../Input/Dropdown/DropdownAnchor';
 
 export interface IDatePickerProps {
+  anchor: IDropdownAnchorProps;
   selectedDate: DateTime | undefined;
   setSelectedDate: (newDate: DateTime | undefined) => void;
 }
 
-export const DatePicker: React.FunctionComponent<IDatePickerProps> = props => {
-  const [currentMonth, setCurrentMonth] = React.useState(
-    DateTime.now().startOf('month')
-  );
-  const [viewType, setViewType] = React.useState<TDatePickerView>('day');
-  const [yearOffset, setYearOffset] = React.useState(0);
-
+export const DatePicker: React.FC<IDatePickerProps> = props => {
   return (
-    <div className='w-[400px]'>
-      <DatePickerHeader
-        currentMonth={currentMonth}
-        setCurrentMonth={setCurrentMonth}
-        viewType={viewType}
-        setViewType={setViewType}
-        yearOffset={yearOffset}
-        setYearOffset={setYearOffset}
-      />
-      {viewType === 'day' && (
-        <DatePickerDayView
-          currentMonth={currentMonth}
-          selectedDate={props.selectedDate}
-          setSelectedDate={props.setSelectedDate}
-        />
-      )}
-      {viewType === 'month' && (
-        <DatePickerMonthView
-          currentMonth={currentMonth}
-          setCurrentMonth={setCurrentMonth}
-          setViewType={setViewType}
-        />
-      )}
-      {viewType === 'year' && (
-        <DatePickerYearView
-          currentMonth={currentMonth}
-          setCurrentMonth={setCurrentMonth}
-          setViewType={setViewType}
-          yearOffset={yearOffset}
-        />
-      )}
-
-      <DatePickerHelperButtons
-        setCurrentMonth={setCurrentMonth}
-        setSelectedDate={props.setSelectedDate}
-      />
-    </div>
+    <Poppable anchor={props.anchor}>
+      <DatePickerInternal {...props} />
+    </Poppable>
   );
 };
