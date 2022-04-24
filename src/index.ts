49ccf4c30297a5a168f8cce160b40539a959b24a
Yaser <me@yaser.ca>
2022-04-24 03:03:39 -0700

    Fixed build

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ export {
   ButtonIcon,
   IButtonProps,
 } from './Components/Button';
-export { TextInput, ITextInputProps } from './Components/Input/TextInput';
-export { FileInput, IFileInputProps } from './Components/Input/FileInput';
-export { Dropdown, IDropdownProps } from './Components/Input/Dropdown/Dropdown';
-export {
-  DatePicker,
-  IDatePickerProps,
-} from './Components/DatePicker/DatePicker';
+export { TextInput } from './Components/Input/TextInput';
+export { FileInput } from './Components/Input/FileInput';
+import { IDropdownProps as IDropdownPropsInternal } from './Components/Input/Dropdown/Dropdown';
+export type IDropdownProps = IDropdownPropsInternal;
+export { Dropdown } from './Components/Input/Dropdown/Dropdown';
+
+export { DatePicker } from './Components/DatePicker/DatePicker';
+import { IDatePickerProps as DP } from './Components/DatePicker/DatePicker';
+export type IDatePickerProps = DP;
